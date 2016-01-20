2d8966cf41b787e496ef2517416b58eeed075a03
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-01-20 23:41:08 +0300

    init

---
+++ index.js
@@ -0,0 +1,50 @@
+module.exports = function(opts) {
+  // Added Angular after data. See https://github.com/mdo/code-guide/issues/106
+  var orderList = opts.order || ['class', 'id', 'name', 'data', 'ng', 'src', 'for', 'type', 'href', 'values', 'title', 'alt', 'role', 'aria'];
+
+  return function(tree) {
+    tree.walk(function(node) {
+      if (node.attrs) {
+        var attrs = Object.keys(node.attrs);
+        if (attrs.length === 1) {
+          return node;
+        }
+
+        var sortAttrs = {};
+        var noSortAttrs = [];
+        attrs = attrs.filter(function(attr) {
+          // The name attribute can be shared (i.e. data-*, aria-*, ng-*),
+          // so used a regular expression
+          for (var i = 0; i < orderList.length; i++) {
+            if (new RegExp(orderList[i]).test(attr)) {
+              return true;
+            }
+          }
+
+          noSortAttrs.push(attr);
+          return false;
+        }).sort(function(a, b) {
+          orderList.forEach(function(item) {
+            if (new RegExp(item).test(a)) {
+              a = item;
+            }
+
+            if (new RegExp(item).test(b)) {
+              b = item;
+            }
+          });
+
+          return orderList.indexOf(a) - orderList.indexOf(b);
+        }).concat(noSortAttrs).forEach(function(attr) {
+          sortAttrs[attr] = node.attrs[attr];
+        });
+
+        node.attrs = sortAttrs;
+      }
+
+      return node;
+    });
+
+    return tree;
+  };
+};
---
+++ test.js
@@ -0,0 +1,65 @@
+var posthtml = require('posthtml');
+var assert = require('assert');
+var m = require('./');
+
+var test = function(input, output, opts, done) {
+  posthtml()
+    .use(m(opts))
+      .process(input)
+      .then(function(result) {
+        assert.equal(result.html, output);
+        done();
+      })
+      .catch(function(error) {
+        done(error);
+      });
+};
+
+it('Standard test', function(done) {
+  test(
+    '<input type="text" class="form-control" name="testInput" autofocus autocomplete="off" id="testId">',
+    '<input class="form-control" id="testId" name="testInput" type="text" autofocus="" autocomplete="off">',
+    {},
+    done
+  );
+});
+
+it('Angular test', function(done) {
+  test(
+    '<a href="#/phones/{{phone.id}}" class="thumb"><img ng-src="{{phone.imageUrl}}" alt="{{phone.name}}"></a>',
+    '<a class="thumb" href="#/phones/{{phone.id}}"><img ng-src="{{phone.imageUrl}}" alt="{{phone.name}}"></a>',
+    {},
+    done
+  );
+});
+
+it('Aria test', function(done) {
+  test(
+    '<span onclick="return checkBoxEvent(event);" role="checkbox" aria-checked="mixed">A checkbox label</span>',
+    '<span role="checkbox" aria-checked="mixed" onclick="return checkBoxEvent(event);">A checkbox label</span>',
+    {},
+    done
+  );
+});
+
+it('Custom config test', function(done) {
+  test(
+    '<a id="testId" href="#" class="testClass"></a>',
+    '<a href="#" id="testId" class="testClass"></a>',
+    {
+      order: ['href', 'id', 'class']
+    },
+    done
+  );
+});
+
+it('Empty config test', function(done) {
+  test(
+    '<a id="testId" href="#" class="testClass"></a>',
+    '<a id="testId" href="#" class="testClass"></a>',
+    {
+      order: []
+    },
+    done
+  );
+});
