aaee44e9cade134147e3ae277135aab5ea03a209
Jon Wolfe <jonathanwolfe5@gmail.com>
2017-02-16 15:22:48 -0500

    Add ability to specify unsorted property location

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ module.exports = function(opts) {
     'data', 'ng', 'src',
     'for', 'type', 'href',
     'values', 'title', 'alt',
-    'role', 'aria'
+    'role', 'aria',
+    '$unknown$'
   ];
 
   // A RegExp's for filtering and sorting
-  var orderListFilterRegExp = new RegExp('(' + orderList.join('|') + ')');
+  var orderListFilterRegExp = new RegExp('^(' + orderList.join('|') + ')');
   var orderListRegExp = orderList.map(function(item) {
     return new RegExp('^' + item);
   });
@@ -21,39 +22,63 @@ module.exports = function(opts) {
       }
 
       var attrs = Object.keys(node.attrs);
-      if (attrs.length === 1) {
+
+      if (attrs.length === 1 || orderList.length === 0) {
         return node;
       }
 
+      var sortableAttrs = [];
       var sortedAttrs = [];
       var notSortedAttrs = [];
-      attrs = attrs
-        // The separation of the attributes on a sortable and not sortable
+      var notSortedAttrsIndex = orderList.indexOf('$unknown$');
+      var finalAttrs = {};
+
+      if (notSortedAttrsIndex === -1) {
+        notSortedAttrsIndex = orderList.length;
+      }
+
+      sortableAttrs = attrs
+        // The separation of the attributes on a sortable and not sortable basis
         .filter(function(attr) {
           if (orderListFilterRegExp.test(attr)) {
             return true;
           }
-          notSortedAttrs.push(attr);
-          return false;
-        })
-        .sort(function(a, b) {
-          orderListRegExp.forEach(function(re, index) {
-            if (re.test(a)) {
-              a = index;
-            }
-            if (re.test(b)) {
-              b = index;
-            }
-          });
 
-          return a - b;
-        })
-        .concat(notSortedAttrs)
-        .forEach(function(attr) {
-          sortedAttrs[attr] = (node.attrs[attr]) ? node.attrs[attr] : true;
+          notSortedAttrs.push(attr);
+
+          return false;
         });
 
-      node.attrs = sortedAttrs;
+      sortedAttrs = orderListRegExp
+        // match to each position
+        .map(function(regex) {
+          return sortableAttrs
+            // attrs that belong in this regex group
+            .filter(function(attr) {
+              return regex.test(attr);
+            })
+            // alpha desc sort each group
+            .sort(function(a, b) {
+              return typeof a.localeCompare === 'function' ? a.localeCompare(b) : a - b;
+            });
+        })
+        // remove empty groups
+        .filter(function(group) {
+          return group.length > 0;
+        });
+
+      sortedAttrs
+        // put the non-sorted attributes in desired slot
+        .splice(notSortedAttrsIndex, 0, notSortedAttrs);
+
+      sortedAttrs.forEach(function(group) {
+        group.forEach(function(attr) {
+          finalAttrs[attr] = (node.attrs[attr]) ? node.attrs[attr] : true;
+        });
+      });
+
+      node.attrs = finalAttrs;
+
       return node;
     });
 
